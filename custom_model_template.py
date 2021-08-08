8f1ff1377b8e168750e387bcce0177ce46ec2815
Daniel Hamacher <danielhamacher.dh@gmail.com>
2021-08-07 20:29:56 -0600

    initial upload of music generation

---
+++ custom_model_template.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+ds_tensors = tf.data.Dataset.from_tensor_slices([1, 2, 3, 4, 5, 6])
+
+# Create a CSV file
+import tempfile
+_, filename = tempfile.mkstemp()
+
+
+
+with open(filename, 'w') as f:
+  f.write("""Line 1
+Line 2
+Line 3
+  """)
+
+ds_file = tf.data.TextLineDataset(filename)
